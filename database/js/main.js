ff6cf76ac8e41ae922fd717af1dc8d272d3eef68
eemiljka <eemiljka@metropolia.fi>
2024-04-02 14:51:30 +0300

    class commit2

--- database/js/main.js
+++ database/js/main.js
@@ -2,6 +2,9 @@
 const modifyLinks = document.querySelectorAll('.modify-link');
 const modifyModal = document.querySelector('#modify-modal');
 const modifyContent = document.querySelector('#modify-content');
+const closeLinks = document.querySelectorAll('.close-modal');
+const successModal = document.querySelector('#success-modal');
+
 
 modifyLinks.forEach((link) => {
     link.addEventListener('click', async (evt) => {
@@ -14,3 +17,18 @@ modifyLinks.forEach((link) => {
         modifyModal.showModal();
     })
 })
+
+closeLinks.forEach((link) => {
+    const parent = link.closest('dialog');
+    link.addEventListener('click', () => {
+        parent.close();
+    })
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('success')) {
+    successModal.showModal();
+    setTimeout(() => {
+        successModal.close();
+    }, 3000);
+}
